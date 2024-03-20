c7b995c48d3fe5542e0a030e52c003670acab588
Ujstor <astipan@gmail.com>
2024-03-20 21:32:02 +0100

    remove old config

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if [ -x "$(command -v nvim)" ]; then
+    sudo rm -rf /usr/local/bin/nvim
+    sudo rm -rf /usr/bin/nvim
+    sudo rm -rf /usr/local/share/nvim
+    sudo rm -rf /usr/share/nvim
+fi
+
+if [ -d ~/.config/nvim ]; then
+    rm -rf ~/.config/nvim
+fi
+
 temp_dir=$(mktemp -d)
 git clone --depth 1 https://github.com/Ujstor/nvim-config.git "$temp_dir"
 
@@ -7,25 +18,12 @@ cd "$temp_dir"
 
 curl -sSL https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz | sudo tar -xz -C /usr/local --strip-components=1
 
-if [ -L /usr/bin/nvim ]; then
-    sudo rm /usr/bin/nvim
-fi
+sudo ln -s /usr/local/bin/nvim /usr/bin/nvim
 
-if [ ! -L /usr/bin/nvim ]; then
-    sudo ln -s /usr/local/bin/nvim /usr/bin/nvim
-else
-    echo "Symbolic link /usr/bin/nvim already exists."
-fi
-
-if [ -d ~/.config/nvim ]; then
-    rm -rf ~/.config/nvim/*
-else
-    mkdir -p ~/.config/nvim
-fi
+mkdir -p ~/.config/nvim
 
 cp -r * ~/.config/nvim/
 
 echo "Neovim setup complete."
 
 rm -rf "$temp_dir"
-
