74dff7cb278536ca208c28c8b2ad50fa95f9fe96
ujstor <astipan@gmail.com>
2025-08-02 19:36:36 +0200

    fix neotree

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -1,25 +1,12 @@
 return {
-  'jiaoshijie/undotree',
-  dependencies = 'nvim-lua/plenary.nvim',
-  config = true,
-  keys = { -- load the plugin only when using it's keybinding:
-    { '<leader>u', "<cmd>lua require('undotree').toggle()<cr>", desc = '[U]ndotree toggle' },
-  },
-  float_diff = true, -- using float window previews diff, set this `true` will disable layout option
-  layout = 'left_bottom', -- "left_bottom", "left_left_bottom"
-  position = 'left', -- "right", "bottom"
-  ignore_filetype = { 'undotree', 'undotreeDiff', 'qf', 'TelescopePrompt', 'spectre_panel', 'tsplayground' },
-  window = {
-    winblend = 30,
-  },
-  keymaps = {
-    ['j'] = 'move_next',
-    ['k'] = 'move_prev',
-    ['gj'] = 'move2parent',
-    ['J'] = 'move_change_next',
-    ['K'] = 'move_change_prev',
-    ['<cr>'] = 'action_enter',
-    ['p'] = 'enter_diffbuf',
-    ['q'] = 'quit',
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      'nvim-tree/nvim-web-devicons', -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
   },
 }
