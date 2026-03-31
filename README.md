40bc281e33e7c2a4d49181e55bfe5d92829add05
ujstor <astipan@gmail.com>
2026-04-01 00:52:56 +0200

    fix nvim 0.12

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
+## Install
+
+```bash
 curl -sSL https://raw.githubusercontent.com/Ujstor/nvim-config/master/install.sh | bash
+```
+
+## Update
+
+After updating Neovim or pulling config changes, sync plugins and parsers:
+
+```
+:Lazy sync
+:TSUpdate
+```
--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -4,37 +4,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     'nvim-tree/nvim-web-devicons',
   },
-  ft = { 'markdown' }, -- Only load for markdown files (optional optimization)
-  ---@module 'render-markdown'
-  ---@type render.md.UserConfig
-  opts = {
-    -- Disable by default
-    enabled = false,
-  },
-  keys = {
-    {
-      '<leader>mr',
-      function()
-        require('render-markdown').toggle()
-      end,
-      desc = 'Toggle [M]arkdown [R]endering',
-      ft = 'markdown',
-    },
-    {
-      '<leader>me',
-      function()
-        require('render-markdown').enable()
-      end,
-      desc = '[M]arkdown [E]nable rendering',
-      ft = 'markdown',
-    },
-    {
-      '<leader>md',
-      function()
-        require('render-markdown').disable()
-      end,
-      desc = '[M]arkdown [D]isable rendering',
-      ft = 'markdown',
-    },
-  },
+  -- Disabled until nvim-treesitter main branch is installed (:Lazy sync + :TSUpdate)
+  -- ft = { 'markdown' } triggers loading which crashes with nvim-treesitter master on Neovim 0.12
+  enabled = false,
 }
